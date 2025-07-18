769582c49b683dce156ad3bb40a0f87e6b00ab1a
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-18 21:15:16 +0900

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -55,7 +55,10 @@ createManyPeople(people, (err, data) => {
 });
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    Person.find({name: personName}, (err, data) => {
+        if (err) return done(err); 
+        return done(null, data); 
+    });
 };
 
 const findOneByFood = (food, done) => {
