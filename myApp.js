a0745c67baa9ed2ff650f2a538b9d282c26ba700
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-19 17:30:47 +0900

    Use Model.findOneAndUpdate() to updates documents

--- myApp.js
+++ myApp.js
@@ -92,8 +92,16 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+    
+  Person.findOneAndUpdate(
+    {name: personName}, 
+    {age: ageToSet}, 
+    {new: true }, 
+    (err, person) => {
+    if (err) return done(err);
+    if (!person) return done(new Error("Person not found")); 
+    return done(null, person); 
+  });
 };
 
 const removeById = (personId, done) => {
